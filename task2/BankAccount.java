4ccf5ea443584b35d90e71a71ab7587b27fdb2d7
grin <grin@kth.se>
2023-09-15 11:00:36 +0200

    Class prepared

---
+++ task2/BankAccount.java
@@ -0,0 +1,49 @@
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount. Amount must be greater than 0.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount. Amount must be greater than 0.");
+        } else {
+            System.out.println("Insufficient balance for withdrawal.");
+        }
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Balance: $" + balance);
+    }
+}
---
+++ task2/task2.md
@@ -0,0 +1,42 @@
+# **Övning DD1337 Programmering - Övning 2**
+Mathias Grindsäter (grin@kth.se)
+
+## 💬**Idag**
+* Viktiga punkter från task 1
+* Presentera era lösningar
+* RAST
+* Övningar
+
+
+## 💬**DD1337 Lösningar**
+* Presentera era lösningar!
+
+## **Rast!**
+
+## **Övningar**
+### 1. Gå igenom Car.java och hitta felen!
+
+### 2. BankAccount.java
+
+1) Skapa en klass `BankAccount` som har tre fält: `accountNumber`,
+`accountHolderName` och `balance`. Låt `accountNumber` vara av typen
+`String`.
+
+2) Skapa en konstruktor som ger initiala värden till de tre fälten.
+3) Skapa tre getters-metoder.
+4) Skapa 2 setters-metoder (`balance` ska inte ha en setter ).
+5) Skapa en metod `deposit`. När man kallar på metoden sätter man in
+en given summa som argument. Fältet `balance` ska då uppdateras.
+6) I metoden `deposit`, hur kan vi hindra att man inte kan sätta in negativa
+summor? 
+7) Skapa en metod `withdraw`. När man kallar på metoden anger
+man hur stor summa man vill ta ut från sitt konto. Se till att
+variabeln `balance` uppdateras.
+8) I metoden `withdraw`, hur ser vi till att vi inte kan ta ut negativa
+summor från bankkontot. Se även till att man inte kan
+ta ut mer pengar än vad som finns på kontont (dvs `balance`)
+9) Skapa ett eller flera objekt i `main`-metoden och kalla
+på de metoder du skapat.
+10) Om du är färdig kan du börja kolla in task 3 :)
+
+
